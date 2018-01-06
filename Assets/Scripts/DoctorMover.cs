d00cb9a31d80a56f299d6b2b9d76b432e4059121
NAD.QC.CA\xpro <aymenamous@gmail.com>
2018-01-06 01:58:37 -0500

    DoctorMover ready

---
+++ Assets/Scripts/DoctorMover.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoctorMover : MonoBehaviour {
+
+	public Camera cam;
+
+	//Horizontal1 pour Doctor et Horizontal2 pour jester
+	public string x = "LeftStickHorizontal1";
+
+	//Vertical1 pour Doctor et Vertical2 pour Jester
+	public string z = "LeftStickVertical1";
+
+	//Movement speed
+	public float speed;
+	//Jump speed
+	public float jumpVelocity;
+
+	//Pour ne pas permettre double saut
+	private bool jump;
+
+	//Rotation horizontale
+	private float xRotation;
+	//Rotation Verticale
+	private float yRotation;
+
+	//Rigidbody du doctor
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		//Init des rotation a 0
+		xRotation = 0;
+		yRotation = 0;
+		jump = true;
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Move
+		transform.Translate (Input.GetAxis(x)*speed,0,Input.GetAxis(z)*speed);
+		//Incrementer/Decrementer la rotation 
+		xRotation += Input.GetAxis ("RightStickHorizontal1");
+		yRotation -= Input.GetAxis ("RightStickVertical1");
+		//Rotate Body
+		transform.eulerAngles = new Vector3 (0, xRotation, 0);
+		//Rotate Camera
+		cam.transform.eulerAngles = new Vector3 (yRotation,xRotation , 0);
+
+		if (Input.GetButtonDown ("AButton1") && jump) 
+		{
+			rb.velocity = Vector3.up * jumpVelocity;
+		}
+		
+	}	
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "ground" && jump == false) {
+			jump = true;
+		}
+	}
+
+	void OnCollisionExit(Collision other){
+		if (other.gameObject.tag == "ground" && jump == true) {
+			jump = false;
+		}
+	}
+
+
+	void Update(){
+		
+	}
+}
