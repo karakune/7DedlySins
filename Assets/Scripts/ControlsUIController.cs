b0d7b3f3b1c0026c72ed4439963e109fe568f15d
Maxime Di Lullo <massimo.dil@gmail.com>
2018-01-07 15:04:28 -0500

    Added Controls to Level Scene

---
+++ Assets/Scripts/ControlsUIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsUIController : MonoBehaviour {
+
+    public GameObject canvas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Back"))
+        {
+            canvas.SetActive(!canvas.activeSelf);
+        }
+	}
+}
